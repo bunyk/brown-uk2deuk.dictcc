3732bf8f3ce1baae7107e32ecbe4c2c23b754f8e
Taras Bunyk <tbunyk@gmail.com>
2025-06-09 17:22:50 +0200

    Add a convertion script and a selection of data from VESUM

--- README.md
+++ README.md
@@ -1,2 +1,59 @@
 # brown-uk2deuk.dictcc
-A set of tools to convert data of brown-uk/dict_uk to dict.cc inflections format
+A set of tools to convert data of [brown-uk/dict_uk](https://github.com/brown-uk/dict_uk/) to [deuk.dict.cc](https://deuk.dict.cc/) inflections format.
+
+Current data is based on the VESUM v6.6.5. (c) Рисін А., Старко В. Великий електронний словник української мови (ВЕСУМ). 2005-2025.
+
+Many thanks for their generous permission to use their dictionary data for this project.
+
+To get data yourself, clone this and dict_uk repositories:
+
+```
+git clone https://github.com/brown-uk/dict_uk.git
+git clone https://github.com/bunyk/brown-uk2deuk.dictcc.git
+```
+
+Then build dict_uk according to it's instructions depending on your system. I did:
+
+```
+cd dict_uk
+docker build -t brown-uk/dict_uk .
+docker run -d --name dict_uk brown-uk/dict_uk /bin/bash
+docker cp dict_uk:/src/out/ ./out
+docker stop dict_uk
+```
+
+This will create `dict_uk/out/out/dict_corp_lt.txt` file looking like this:
+
+```
+а а conj:coord
+...
+millions more of similar lines
+...
+ящурні ящурний adj:p:v_kly
+```
+
+Format for each line of that file is (lexem, space, lexem, space, colon separated tags).
+
+To generate inflections for nouns, adjectives and verbs, run the following commands:
+
+```
+# generate nouns in "nom.sg | gen.sg | pl" format:
+python brown-uk2deuk.dictcc/convert.py noun dict_uk/out/out/dict_corp_lt.txt | tee brown-uk2deuk.dictcc/nouns.txt
+
+# generate adjectives in "m | n | f | pl" format:
+python brown-uk2deuk.dictcc/convert.py adj dict_uk/out/out/dict_corp_lt.txt | tee brown-uk2deuk.dictcc/adjectives.txt
+
+# generate verbs in 
+#   infinitive | present.1p.sg (imperfective verbs) | past
+# or
+#    infinitive | future.1p.sg (perfective verbs) | past
+# formats, depending on the verb aspect:
+python brown-uk2deuk.dictcc/convert.py verb dict_uk/out/out/dict_corp_lt.txt | tee brown-uk2deuk.dictcc/verbs.txt
+```
+
+Currently repository based on VESUM v6.6.5 contains:
+
+   86110 adjectives.txt
+  173000 nouns.txt
+   27198 verbs.txt
+  286308 total
---
+++ convert.py
@@ -0,0 +1,105 @@
+"""
+A script to process a dictionary file of VESUM and convert it to dict.cc format for inflections.
+
+For documentation on tags selected, see:
+https://github.com/brown-uk/dict_uk/blob/master/doc/tags.txt
+"""
+
+USAGE = "Usage: python convert.py <adj|noun|verb> <filename>"
+from itertools import islice
+from sys import argv
+from collections import defaultdict
+
+def main():
+    if len(argv) != 3:
+        print(USAGE)
+        return
+
+    selectors = {
+        'noun': Nouns(),
+        'verb': Verbs(),
+        'adj': Adjectives(),
+    }
+    if argv[1] not in selectors:
+        print(USAGE)
+        return
+
+    selector = selectors[argv[1]]
+    
+    with open(argv[2]) as dict_uk:
+        for line in dict_uk:
+            line = line.strip()
+            lexem, lemma, tags = line.split(' ')
+            tags = set(tags.split(':'))
+
+            if {'bad', 'rare', 'arch', 'alt'}.intersection(tags):
+                continue # skip errorneous, rare, achaic and non-normative forms
+
+            selector.add(lexem, lemma, tags)
+
+    selector.print()
+
+class Selector:
+    def __init__(self):
+        self.words = defaultdict(dict)
+
+    def print(self):
+        for lemma, forms in self.words.items():
+            print(self.format(lemma, forms))
+
+class Nouns(Selector):
+    def add(self, lexem, lemma, tags):
+        if 'noun' not in tags:
+            return
+        if 'p' not in tags and 'v_rod' in tags: # take singular genitive
+            self.words[lemma]['gen.sg'] = lexem
+        if 'p' in tags and 'v_naz' in tags: # and plural nominative
+            self.words[lemma]['pl'] = lexem
+
+    def format(self, lemma, forms):
+        return f"{lemma} | {forms.get('gen.sg', '-')} | {forms.get('pl', '-')}"
+
+class Adjectives(Selector):
+    def add(self, lexem, lemma, tags):
+        if 'adj' not in tags:
+            return
+        if 'compc' in tags or 'comps' in tags:
+            return # only basic form (skip comparatives & superlatives)
+        if 'v_naz' not in tags:
+            return # only nominative
+
+        for form in ['m', 'n', 'f', 'p']:
+            if form not in tags:
+                continue
+            self.words[lemma][form] = lexem
+
+    def format(self, lemma, forms):
+        return f"{forms.get('m', '-')} | {forms.get('n', '-')} | {forms.get('f', '-')} | {forms.get('p', '-')}"
+
+class Verbs(Selector):
+    def add(self, lexem, lemma, tags):
+        if 'verb' not in tags:
+            return
+
+        if tags == {'verb', 'imperf', 'inf'}:
+            self.words[lemma]['imperf_inf'] = lexem
+        elif tags == {'verb', 'imperf', 'pres', 's', '1'}:
+            self.words[lemma]['present.1p.sg'] = lexem
+        elif tags == {'verb', 'imperf', 'past', 'm'}:
+            self.words[lemma]['past'] = lexem
+
+        elif tags == {'verb', 'perf', 'inf'}:
+            self.words[lemma]['perf_inf'] = lexem
+        elif tags == {'verb', 'perf', 'futr', 's', '1'}:
+            self.words[lemma]['future.1p.sg'] = lexem
+        elif tags == {'verb', 'perf', 'past', 'm'}:
+            self.words[lemma]['past'] = lexem
+
+    def format(self, lemma, forms):
+        if 'imperf_inf' in forms:
+            return f"{lemma} | {forms.get('present.1p.sg', '-')} | {forms.get('past', '-')}"
+        elif 'perf_inf' in forms:
+            return f"{lemma} | {forms.get('future.1p.sg', '-')} | {forms.get('past', '-')}"
+
+if __name__ == "__main__":
+    main()
